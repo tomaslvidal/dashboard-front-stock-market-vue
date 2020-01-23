50baa310d8f8daa656d37db49c5d3b0b8b1282b2
cheqeeh <vtcheqeeh@gmail.com>
2020-01-23 07:34:24 -0300

    up

--- src/main.js
+++ src/main.js
@@ -1,8 +1,18 @@
 import Vue from 'vue'
+
+import { BootstrapVue } from 'bootstrap-vue'
+
 import App from './App.vue'
+
 import './registerServiceWorker'
+
 import router from './router'
+
 import store from './store'
+
+import '@/scss/custom.scss'
+
+Vue.use(BootstrapVue);
 
 Vue.config.productionTip = false
 
