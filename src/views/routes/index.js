bf58ef6054bfe4940df2fc952cacd1a0ada95ed7
cheqeeh <vtcheqeeh@gmail.com>
2020-02-08 12:32:27 -0300

    Update index.js

--- src/views/routes/index.js
+++ src/views/routes/index.js
@@ -1,3 +1,11 @@
 import Routes from './routes';
 
+import Buy from './buy';
+
+import Config from './config';
+
+import Leverage from './leverage';
+
+export { Buy, Config, Leverage };
+
 export default Routes;
