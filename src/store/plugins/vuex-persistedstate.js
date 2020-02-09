0c91dae9436a3db632e4d05df3dbce74c92566dd
cheqeeh <vtcheqeeh@gmail.com>
2020-02-09 17:10:59 -0300

    Update vuex-persistedstate.js

--- src/store/plugins/vuex-persistedstate.js
+++ src/store/plugins/vuex-persistedstate.js
@@ -1,20 +1,8 @@
 import Plugin from 'vuex-persistedstate';
 
-import SecureLS from "secure-ls";
-
-import AES from 'crypto-js/aes';
-
-import enc_utf8 from 'crypto-js/enc-utf8';
-
-const ls = new SecureLS({ encodingType: 'aes' });
-
 const VuexPersistedstate = Plugin({
     key: 'root',
-    storage: {
-        getItem: key => ls.get(key),
-        setItem: (key, value) => ls.set(key, value),
-        removeItem: key => ls.remove(key)
-    }
+    storage: localStorage
 });
 
 export default VuexPersistedstate;
