b1fae723d0c980f9c5c7b13a304b4fb4fd22e701
cheqeeh <vtcheqeeh@gmail.com>
2020-02-09 17:10:57 -0300

    Update leverage.vue

--- src/views/routes/leverage/leverage.vue
+++ src/views/routes/leverage/leverage.vue
@@ -4,13 +4,13 @@
             <div class="form-group col-6">
                 <label for="start_date">Fecha inicial</label>
 
-                <input class="form-control form-control-sm" type="number" name="start_date" id="start_date">
+                <input v-model="form.start_date" class="form-control form-control-sm" type="number" name="start_date" id="start_date">
             </div>
 
             <div class="form-group col-6">
-                <label for="pesos_capital">T.E.A</label>
+                <label for="tea">T.E.A</label>
 
-                <input class="form-control form-control-sm" type="number" name="pesos_capital" id="pesos_capital">
+                <input v-model="form.tea" class="form-control form-control-sm" type="number" name="tea" id="tea">
             </div>
         </div>
 
@@ -21,10 +21,20 @@
 
             <div class="d-flex w-100 pl-3" id="dollars_to_pesos">
                 <div class="form-group form-check col-12">
-                    <input type="checkbox" class="form-check-input" id="detector">
+                    <input v-model="form.detector" type="checkbox" class="form-check-input" id="detector">
 
                     <label class="form-check-label" for="detector">Detector</label>
                 </div>
+            </div>
+        </div>
+
+        <hr>
+
+        <div class="form-row">
+            <div class="col-3">
+                <button @click.stop.prevent="onSubmit" class="btn btn-sm btn-primary w-100">
+                    Guardar
+                </button>
             </div>
         </div>
     </div>
@@ -32,5 +42,18 @@
 
 <script>
     export default {
+        computed: {
+            form(){
+                return Object.assign({}, this.$store.state.Root.leverage);
+            }
+        },
+        methods:{
+            onSubmit(){
+                this.$store.commit('Root/SET_FORM', {
+                    'form': 'leverage',
+                    'data': JSON.parse(JSON.stringify(this.form))
+                });
+            }
+        }
     }
 </script>
