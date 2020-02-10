1f71401f26a087fc9604b906b70d30fd961b46bc
cheqeeh <vtcheqeeh@gmail.com>
2020-02-10 02:00:55 -0300

    Update default-layout.vue

--- src/layouts/default/default-layout.vue
+++ src/layouts/default/default-layout.vue
@@ -13,13 +13,13 @@
             class="root"
             v-else
         >
-            <div class="col-xl-2 col-lg-4 col-12  bg-dark aside">
+            <div class="col-md-3 d-none d-md-block bg-dark aside">
                 <aside>
                     <router-view name="aside" />
                 </aside>
             </div>
 
-            <div class="col-xl-10 col-lg-8 col-md-12 bg-light content">
+            <div class="col-md-9 col-12 bg-light content">
                 <router-view name="header" />
 
                 <main class="bg-white p-3 my-4 mx-2 rounded">
@@ -51,30 +51,30 @@
         components: {
             Skeleton
         },
-        data(){
-            return{
-                components: {}
-            };
-        },
         computed:{
             assert_with_aside_content(){
-                if(typeof this.$store.state.Router !== 'undefined'){
-                    let router = this.$router.options.routes.filter(item => item.path === this.$store.state.Router.path);
+                let routes = this.$router.options.routes;
 
-                    if(router.length){
-                        router = router[0];
-                        
-                        if(typeof router.components !== 'undefined'){
-                            router = router.components;
-                            
-                            if(typeof router.aside !== 'undefined' && typeof router.content !== 'undefined'){
-                                return true;
-                            }
+                const motor = routes => {
+                    let state = false;
+
+                    for (let index = 0; index < routes.length; index++) {
+                        if(routes[index].name === this.$store.state.Router.name){
+                            state = true;
+                        }
+                        else if(typeof routes[index].children !== 'undefined'){
+                            state = motor(routes[index].children);
+                        }
+
+                        if(state){
+                            break;
                         }
                     }
+
+                    return state;
                 }
 
-                return false;
+                return motor(this.$router.options.routes);
             }
         }
     }
