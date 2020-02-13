6651850dd0d8114e55044974e0218e638b1967d4
cheqeeh <vtcheqeeh@gmail.com>
2020-02-13 00:32:56 -0300

    Update header.vue

--- src/views/components/header/header.vue
+++ src/views/components/header/header.vue
@@ -1,4 +1,5 @@
 <template>
+<!-- <header data-v-168f7fa0="" data-v-75b77123="" class="header col-12 bg-primary d-flex align-items-center"><span data-v-168f7fa0="" class="text-light d-flex align-items"><i data-v-168f7fa0="" class="fas fa-bars menu pl-1" style="cursor: pointer; font-size: 25px;"></i></span></header> -->
     <header class="header row bg-primary">
         <div class="col-12">
             <div class="d-flex h-100 justify-content-start align-items-center">
